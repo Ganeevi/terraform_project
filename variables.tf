84baf2da7a4ce99e9a8ae15bbbb83babebc47673
Ganeevi <131597929+Ganeevi@users.noreply.github.com>
2024-04-24 18:14:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,43 +1,79 @@
+variable "aws_region" {
+    description = "value"
+    type = map(string)
+    default = {
+      "dev" = "ap-south-1"
+      "stage" = "ap-northeast-1"
+      "prod" = "us-east-1"
+    }
+}
+
 variable "vpc_cidr" {
-    //default =  "10.0.0.0/16"
+    description = "value"
+    type = map(string)
+    default = {
+      "dev" = "10.0.0.0/16"
+      "stage" = "172.16.0.0/16"
+      "prod" = "192.168.0.0/16"
+    }
 }
 
-variable "AMI_id" {
-    //default = "ami-0451f2687182e0411"
+variable "Public-Subnet-1" {
+    description = "value"
+    type = map(string)
+    default = {
+      "dev" = "ap-south-1a"
+      "stage" = "ap-northeast-1a"
+      "prod" = "us-east-1a"
+    }
 }
 
-variable "subnet_cidr-1a" {
-    //default =  "10.0.10.0/24"
+variable "Public-Subnet-2" {
+    description = "value"
+    type = map(string)
+    default = {
+      "dev" = "ap-south-1b"
+      "stage" = "ap-northeast-1c"
+      "prod" = "us-east-1b"
+    }
 }
 
-variable "AZ-1a" {
-    //default = "ap-south-1a"
+variable "subnet-1_cidr" {
+    description = "value"
+    type = map(string)
+    default = {
+      "dev" = "10.0.10.0/24"
+      "stage" = "172.16.10.0/24"
+      "prod" = "192.168.10.0/24"
+    }
 }
 
-variable "subnet_cidr-1b" {
-    //default =  "10.0.20.0/24"
+variable "subnet-2_cidr" {
+    description = "value"
+    type = map(string)
+    default = {
+      "dev" = "10.0.20.0/24"
+      "stage" = "172.16.20.0/24"
+      "prod" = "192.168.20.0/24"
+    }
 }
 
-variable "AZ-1b" {
-    //default = "ap-south-1b"
+variable "instance_type" {
+    description = "value"
+    type = map(string)
+    default = {
+      "dev" = "t2.micro"
+      "stage" = "t2.micro"
+      "prod" = "t2.micro"
+    }
 }
 
-variable "instance_type-Jenkins" {
-    //default = "t2.micro"
-}
-
-variable "instance_type-jfrog" {
-    //default = "t2.medium"
-}
-
-variable "repository_name" {
-  //description = "The name of the repository"
-  //type        = string
-  //default     = "myrepo"
-}
-
-variable "repository_type" {
-  description = "The type of repository to create. Either `public` or `private`"
-  type        = string
-  default     = "private"
+variable "ami_id" {
+    description = "value"
+    type = map(string)
+    default = {
+      "dev" = "ami-060f2cb962e997969"
+      "stage" = "ami-04e0b6d6cfa432943"
+      "prod" = "ami-0a1179631ec8933d7"
+    }
 }
