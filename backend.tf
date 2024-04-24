62ecef3c20a2b5158c59ee14aa2091e4743a92ca
Ramandeep Singh <reyman.raman@gmail.com>
2024-04-24 18:31:34 +0530

    commenting out backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket = "terraform-init-april-training"
     region = "ap-south-1"
     key = "raman/terraform.tfstate"
     dynamodb_table = "terraform_lock"
   }
-}
+}*/
