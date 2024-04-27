376ef830d3e540d340253f83dc1304e217d2bb17
Ganeevi <131597929+Ganeevi@users.noreply.github.com>
2024-04-27 23:22:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-/*terraform {
+terraform {
   backend "s3" {
-    bucket = "terraform-init-april-training"
+    bucket = "grewall-s3-demo-xyz"
     region = "ap-south-1"
     key = "raman/terraform.tfstate"
     dynamodb_table = "terraform_lock"
   }
-}*/
+}
