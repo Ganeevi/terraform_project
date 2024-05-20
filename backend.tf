fce08af2128a6b6d06739a3f21a93cd9cd7c3a04
Ramandeep Singh <reyman.raman@gmail.com>
2024-05-20 20:25:25 +0530

    Enabling Backend details

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-/*terraform {
+terraform {
   backend "s3" {
-    bucket         = "practicing-jenkins-with-terraform"
-    region         = "ap-south-1"
+    bucket         = "terraform-backend-1466"
+    region         = "us-east-1"
     key            = "raman/terraform.tfstate"
     dynamodb_table = "terraform_lock1"
   }
-}*/
+}
